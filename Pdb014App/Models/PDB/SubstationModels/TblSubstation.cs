8176ad1c2ca21942ac61195ee594d0d44450d167
moshiur-cse <moshiur_cse@hotmail.com>
2020-04-15 11:30:55 +0600

    Add new colum in Substration By ARH

--- Pdb014App/Models/PDB/SubstationModels/TblSubstation.cs
+++ Pdb014App/Models/PDB/SubstationModels/TblSubstation.cs
@@ -91,6 +91,8 @@ namespace Pdb014App.Models.PDB.SubstationModels
         [Display(Name = "Longitude")]
         public decimal? Longitude { get; set; }
 
+
+        //Added By Anisur Rahman
         [Column("DefaultZoomLevel", Order = 12, TypeName = "int")]
         [DataType(DataType.Text)]
         [Display(Name = "Default Zoom Level")]
